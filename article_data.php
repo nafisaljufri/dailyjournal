b7b04cc763df4a064912d8a73ab6f040b1e3c729
nafisaljufri <nafisaljufri@gmail.com>
2024-12-26 19:56:24 +0700

    first commit

---
+++ article_data.php
@@ -0,0 +1,94 @@
+<table class="table table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>No</th>
+                        <th class="w-25">Judul</th>
+                        <th class="w-75">Harga</th>
+                        <th class="w-25">Gambar</th>
+                        <th class="w-25">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    include "koneksi.php";
+
+                    $hlm = (isset($_POST['hlm'])) ? $_POST['hlm'] : 1;
+                    $limit = 2;
+                    $limit_start = ($hlm - 1) * $limit;
+                    $no = $limit_start + 1;
+
+                    $sql = "SELECT * FROM article ORDER BY tanggal DESC LIMIT $limit_start, $limit";
+                    $hasil = $conn->query($sql);
+
+                    $no = 1;
+                    while ($row = $hasil->fetch_assoc()) {
+                    ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td>
+                                <strong><?= $row["judul"] ?></strong>
+                                <br>pada : <?= $row["tanggal"] ?>
+                                <br>oleh : <?= $row["username"] ?>
+                            </td>
+                            <td><?= $row["harga"] ?></td>
+                            <td>
+                                <?php
+                                if ($row["gambar"] != '') {
+                                    if (file_exists('image/' . $row["gambar"] . '')) {
+                                ?>
+                                        <img src="image/<?= $row["gambar"] ?>" width="100">
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <a href="#" title="edit" class="badge rounded-pill text-bg-success" data-bs-toggle="modal" data-bs-target="#modalEdit<?= $row["id"] ?>"><i class="bi bi-pencil"></i></a>
+                                <a href="#" title="delete" class="badge rounded-pill text-bg-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $row["id"] ?>"><i class="bi bi-x-circle"></i></a>                       
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+
+            <?php 
+$sql1 = "SELECT * FROM article";
+$hasil1 = $conn->query($sql1); 
+$total_records = $hasil1->num_rows;
+?>
+<p>Total article : <?php echo $total_records; ?></p>
+<nav class="mb-2">
+    <ul class="pagination justify-content-end">
+    <?php
+        $jumlah_page = ceil($total_records / $limit);
+        $jumlah_number = 1; //jumlah halaman ke kanan dan kiri dari halaman yang aktif
+        $start_number = ($hlm > $jumlah_number)? $hlm - $jumlah_number : 1;
+        $end_number = ($hlm < ($jumlah_page - $jumlah_number))? $hlm + $jumlah_number : $jumlah_page;
+
+        if($hlm == 1){
+            echo '<li class="page-item disabled"><a class="page-link" href="#">First</a></li>';
+            echo '<li class="page-item disabled"><a class="page-link" href="#"><span aria-hidden="true">&laquo;</span></a></li>';
+        } else {
+            $link_prev = ($hlm > 1)? $hlm - 1 : 1;
+            echo '<li class="page-item halaman" id="1"><a class="page-link" href="#">First</a></li>';
+            echo '<li class="page-item halaman" id="'.$link_prev.'"><a class="page-link" href="#"><span aria-hidden="true">&laquo;</span></a></li>';
+        }
+
+        for($i = $start_number; $i <= $end_number; $i++){
+            $link_active = ($hlm == $i)? ' active' : '';
+            echo '<li class="page-item halaman '.$link_active.'" id="'.$i.'"><a class="page-link" href="#">'.$i.'</a></li>';
+        }
+
+        if($hlm == $jumlah_page){
+            echo '<li class="page-item disabled"><a class="page-link" href="#"><span aria-hidden="true">&raquo;</span></a></li>';
+            echo '<li class="page-item disabled"><a class="page-link" href="#">Last</a></li>';
+        } else {
+        $link_next = ($hlm < $jumlah_page)? $hlm + 1 : $jumlah_page;
+            echo '<li class="page-item halaman" id="'.$link_next.'"><a class="page-link" href="#"><span aria-hidden="true">&raquo;</span></a></li>';
+            echo '<li class="page-item halaman" id="'.$jumlah_page.'"><a class="page-link" href="#">Last</a></li>';
+        }
+    ?>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,360 @@
+<?php
+include "koneksi.php"; 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Adidas</title>
+    <link rel="icon" href="image/adidas-logo.png"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <style>
+        .nav-item a:hover {
+        border-bottom: 3px solid black;
+        padding-bottom: 0cm;
+        }
+  </style>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- nav begin -->
+    <nav>
+
+    </nav>
+    <!-- nav end -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="image/adidaslogo.jpg" width="280">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0 text-dark">
+              <li class="nav-item ms-5">
+                <a class="nav-link" aria-current="page" href="#gallery"><b>PRIA</b></a>
+              </li>
+              <li class="nav-item ms-5">
+                <a class="nav-link" href="#article"><b>WANITA</b></a>
+              </li>
+              <li class="nav-item ms-5">
+                <a class="nav-link" href="#article"><b>ANAK</b></a>
+              </li>
+              <li class="nav-item1 ms-5 dropdown">
+                <a class="nav-link dropdown-toggle" href="#gallery" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <b>PROMO</b>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#gallery">Pria</a></li>
+                  <li><a class="dropdown-item" href="#article">Wanita</a></li>
+                  <li><br class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#gallery" style="font-size: x-small;"><b>Lihat Lainnya</b></b></a></li>
+                </ul>
+              </li>
+              <li class="nav-item ms-5">
+                <a class="nav-link" href="login.php" target"_blank"><b>LOGIN</b></a>
+              </li>
+              <li class="nav-item ms-5">
+                <a class="nav-link" href="logout.php" target"_blank"><b>LOGOUT</b></a>
+              </li>
+            </ul>
+            <form class="d-flex m-2" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+            <div class="darkmode d-flex">
+              <div>
+              <button id="darkbutton" type="button" class="btn btn-dark m-1">
+                <i class="bi bi-moon-fill"></i>
+              </button>
+            </div>
+            <div>
+              <button id="lightbutton" type="button" class="btn btn-outline-dark m-1">
+                <i class="bi bi-brightness-high-fill"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        </div>
+      </nav>
+    <!-- hero begin -->
+    <section id="hero" class="isi text-center p-5 bg-warning-subtle text-sm-start">
+        <div class="container">
+        <div class="d-sm-flex flex-sm-row-reverse align-items-center">
+                <img src="image/admd.jpg" class="img-fluid" width="350">
+                <img src="image/admd2.jpg" class="img-fluid" width="350">
+            <div class="p-5">
+                <h1 class="fw-bold display-4">
+                    TEMUKAN KOLEKSI TENNIS-LUXE!
+                </h1>
+                <h4 class="lead display-6">
+                    Lengkapi hobi kamu dengan berbagai fashion tennis.
+                </h4>
+                <br>
+                <div>
+                  <a href="#article" class="btn btn-light p-3" type="button"><b>BELANJA SEKARANG 🡭</b></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- hero end -->
+<!-- article begin -->
+<section id="article" class="text-left p-5">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center">
+      <?php
+      $sql = "SELECT * FROM article ORDER BY tanggal DESC";
+      $hasil = $conn->query($sql); 
+
+      while($row = $hasil->fetch_assoc()){
+      ?>
+        <div class="col">
+          <div class="card h-100">
+            <img src="image/<?= $row["gambar"]?>" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title"><?= $row["judul"]?></h5>
+              <p class="card-text">
+                <?= $row["harga"]?>
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-body-secondary">
+                <?= $row["tanggal"]?>
+              </small>
+            </div>
+          </div>
+        </div>
+        <?php
+      }
+      ?> 
+    </div>
+  </div>
+</section>
+<!-- article end -->
+    <!-- gallery begin -->
+    <section id="gallery" class="text-center p-4 bg-secondary-subtle">
+        <div class="container">
+            <div id="carouselExampleCaptions" class="carousel slide">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="image/Adidas-java.jpg" class="d-block w-100">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>ADIDAS JAVA COLLECTION</h5>
+                    <p>Menghadirkan kembali sepatu ikonis OG adidas Java dipadukan dengan Java tee yang penuh warna dan Java bag yang elegan, bikin penampilan kamu makin maksimal.​</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="image/Aidas-taek.jpg" class="d-block w-100">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>TAEKWONDO-INSPIRED SHOES</h5>
+                    <p>Sebuah gaya awal tahun 00-an dengan desain slip-on yang minimalis.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="image/JJJJound.jpg" class="d-block w-100">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>JJJJound & ADIDAS ORIGINALs</h5>
+                    <p>Terinspirasi oleh penampilan warisan sepak bola di luar lapangan.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="image/Adidas-walesbonner.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>ADIDAS WALES BONNER</h5>
+                    <p>Musim ini, adidas Originals dan Wales Bonner melanjutkan kemitraan kolaborasi berkelanjutan mereka dengan koleksi Musim Semi/Musim Panas 2023.</p>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+        </div>
+    </section>
+    <!-- gallery end -->
+    <!-- footer begin -->
+    <footer class="ms-auto p-5 text-center">
+      <div class="container-fluid">
+        <div class="d-sm-flex flex-sm-row">
+          <img src="image/adidas-logo.png" width="100">
+            <div class="p-5 ms-auto">
+              <a href="https://www.instagram.com/adidas/"><i class="p-2 text-dark bi bi-instagram"></i></a>
+              <a href="https://www.x.com/adidas"><i class="p-2 text-dark bi bi-twitter-x"></i></a>
+              <a href="https://www.facebook.com/adidas/"><i class="p-2 h-2 text-dark bi bi-facebook"></i></a>
+              <a href="https://www.youtube.com/adidas/"><i class="p-2 h-2 text-dark bi bi-youtube"></i></a>
+            </div>
+        </div>
+        <hr>
+        <section id="schedule" class="text-center p-3">
+          <h1 class="fw-bold text-center display-4 pb-4">Schedule</h1>
+          <div class="container">
+              <div class="row row-cols-1 row-cols-md-3 g-3 text-center">
+                  <div class="col">
+                   <a class="nav-link active h-100" href="#">
+                      <div class="card text-light" style="height: 15rem;width: 25rem;">
+                          <div class="card-header bg-danger">
+                            Senin
+                          </div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Basis Data</li>
+                            <li class="list-group-item">Rekayasa Perangkat Lunak</li>
+                          </ul>
+                        </div>
+                   </a>
+                  </div>
+                  <div class="col">
+                      <a class="nav-link active h-100" href="#">
+                         <div class="card text-light" style="height: 15rem;width: 25rem;">
+                             <div class="card-header bg-danger">
+                               Selasa
+                             </div>
+                             <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Pemrograman Berbasis Web</li>
+                               <li class="list-group-item">Pendidikan Pancasila</li>
+                             </ul>
+                           </div>
+                      </a>
+                     </div>
+                     <div class="col">
+                      <a class="nav-link active h-100" href="#">
+                         <div class="card text-light" style="height: 15rem;width: 25rem;">
+                             <div class="card-header bg-danger">
+                               Rabu
+                             </div>
+                             <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Logika Informatika</li>
+                               <li class="list-group-item">Probabilitas dan Statistik</li>
+                             </ul>
+                           </div>
+                      </a>
+                     </div>
+                     <div class="col">
+                      <a class="nav-link active h-100" href="#">
+                         <div class="card text-light" style="height: 15rem;width: 25rem;">
+                             <div class="card-header bg-danger">
+                               Kamis
+                             </div>
+                             <ul class="list-group list-group-flush">
+                               <li class="list-group-item">Basis Data</li>
+                               <li class="list-group-item">Sistem Operasi</li>
+                             </ul>
+                           </div>
+                      </a>
+                     </div>
+                     <div class="col">
+                      <a class="nav-link active h-100" href="#">
+                         <div class="card text-light" style="height: 15rem;width: 25rem;">
+                             <div class="card-header bg-danger">
+                               Jumat
+                             </div>
+                             <ul class="list-group list-group-flush">
+                               <li class="list-group-item text-danger">Libur</li>
+                             </ul>
+                           </div>
+                      </a>
+                     </div>
+                     <div class="col">
+                      <a class="nav-link active h-100" href="#">
+                         <div class="card text-light" style="height: 15rem;width: 25rem;">
+                             <div class="card-header bg-danger">
+                               Sabtu
+                             </div>
+                             <ul class="list-group list-group-flush">
+                               <li class="list-group-item text-danger">Libur</li>
+                             </ul>
+                           </div>
+                      </a>
+                     </div>
+                     </div>
+                  </div>
+                </div>
+          </div>
+      </section>
+      <!-- hero end -->
+      <!-- aboutme -->
+      <section id="aboutme" class="text-center p-5 bg-danger-subtle text-sm-start">
+          <div class="container justify-content-center">
+          <div class="d-sm-flex flex-sm-row ms-5 align-items-center">
+                  <img src="https://lh3.googleusercontent.com/a/ACg8ocJJNxZOtk6i4LnCRpVW0g4RZlpD0O7rqi1pDO6yBzRvFcVU7qny=s360-c-no" class="img-fluid rounded-circle" width="300">
+              <div class="p-5">
+                  <h5 class="lead">
+                      A11.2023.15328
+                  </h5>
+                  <h1 class="fw-bold display-4">
+                      Muchamad Nafis Aljufri
+                  </h1>
+                  <h6>
+                      Program Studi Teknik Informatika
+                  </h6>
+                  <h6 class="fw-bold">
+                      Universitas Dian Nuswantoro
+                  </h6>
+                  <br>
+              </div>
+          </div>
+      </section>
+      <!-- footer begin -->
+      <footer class="p-5 text-center">
+                <div>
+                  <a href="https://www.instagram.com/nafisaljufrii/"><i class="text-dark bi bi-instagram"></i></a>
+                  <a href="https://www.x.com/adidas"><i class="h-2 p-2 text-dark bi bi-twitter-x"></i></a>
+                  <a href="https://www.facebook.com/nafisaljufri10/"><i class="p-2 h-2 text-dark bi bi-whatsapp"></i></a>
+                </div>
+            </div>
+            <div>
+              © 2024 Muchamad Nafis Aljufri
+            </div>
+    </footer>
+    <!-- footer end -->
+    <script type="text/javascript">
+      document.getElementById("darkbutton").onclick = function () {
+
+        const collection = document.getElementsByClassName("isi");
+        for (let i = 0; i < collection.length; i++){     
+          collection[i].classList.remove("bg-warning-subtle");
+          collection[i].classList.add("bg-dark");
+          collection[i].classList.add("text-white");
+        }
+
+        document.getElementById("gallery").classList.remove("bg-secondary-subtle");
+        document.getElementById("gallery").classList.add("bg-secondary");    
+
+        document.body.classList.add("bg-dark-subtle");
+
+        document.getElementById("card").classList.remove("bg-danger");
+        document.getElementById("card").classList.add("bg-dark"); 
+      }
+      document.getElementById("lightbutton").onclick = function () {
+
+        const collection = document.getElementsByClassName("isi");
+        for (let i = 0; i < collection.length; i++){
+          collection[i].classList.remove("bg-dark");
+          collection[i].classList.add("bg-warning-subtle");
+          collection[i].classList.remove("text-white");
+        }
+
+        document.getElementById("gallery").classList.remove("bg-secondary");
+        document.getElementById("gallery").classList.add("bg-secondary-subtle");
+        
+        document.body.classList.remove("bg-dark-subtle");
+
+        document.getElementById("card").classList.remove("bg-dark");
+        document.getElementById("card").classList.add("bg-danger"); 
+      }
+    </script>
+</body>
+</html>
